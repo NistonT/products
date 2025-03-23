9e60907cb4c38ba9add496326e8b11ab5d57f884
NistonN <nisprogrammer@gmail.com>
2025-03-23 10:43:11 +0300

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	output: "export",
+	basePath: "/products",
 	images: {
 		remotePatterns: [
 			{
@@ -8,6 +10,7 @@ const nextConfig: NextConfig = {
 				hostname: "**",
 			},
 		],
+		unoptimized: true,
 	},
 };
 
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,9 +1,19 @@
+import { productsService } from "@/services/products.service";
+import { IProduct } from "@/store/store";
 import { IdProduct } from "./IdProduct";
 
 interface Props {
 	params: {
 		id: number;
 	};
+}
+
+export async function generateStaticParams() {
+	const products = await productsService.getProducts();
+
+	return products.data.map((product: IProduct) => ({
+		id: product.id.toString(),
+	}));
 }
 
 export default function ProductPage({ params: { id } }: Props) {
