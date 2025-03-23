f5e4e6dfdd2080f64c8ea96fe3fac8dae0e65ae3
niston <122088978+NistonT@users.noreply.github.com>
2025-03-23 13:21:22 +0300

    Update page.tsx

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,20 +1,29 @@
-/* eslint-disable */
 import { productsService } from "@/services/products.service";
 import { IProduct } from "@/store/store";
 import { IdProduct } from "./IdProduct";
 
-interface Params {
+// Тип для параметров маршрутизации
+type Params = {
   id: string;
-}
+};
 
+// Тип для пропсов страницы
+type Props = {
+  params: Params;
+};
+
+// Функция generateStaticParams возвращает массив объектов с id как строкой
 export async function generateStaticParams() {
   const products = await productsService.getProducts();
+
   return products.data.map((product: IProduct) => ({
-    id: product.id.toString(),
+    id: product.id.toString(), // Преобразуем id в строку
   }));
 }
 
-// @ts-expect-error
-export default function ProductPage({ params }: { params: Params }) {
-  return <IdProduct id={Number(params.id)} />;
+// Компонент ProductPage теперь работает с правильными типами
+export default function ProductPage({ params }: Props) {
+  const { id } = params;
+
+  return <IdProduct id={Number(id)} />;
 }
