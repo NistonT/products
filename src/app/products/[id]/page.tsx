ead84cafa5ab2a7ba16ce93a7d4087887fb03480
niston <122088978+NistonT@users.noreply.github.com>
2025-03-23 13:06:19 +0300

    Update page.tsx

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -2,18 +2,15 @@ import { productsService } from "@/services/products.service";
 import { IProduct } from "@/store/store";
 import { IdProduct } from "./IdProduct";
 
-interface Props {
-	params: {
-		id: string;
-	};
+interface Params {
+  id: string;
 }
 
 export async function generateStaticParams() {
-	const products = await productsService.getProducts();
-
-	return products.data.map((product: IProduct) => ({
-		id: product.id.toString(),
-	}));
+  const products = await productsService.getProducts();
+  return products.data.map((product: IProduct) => ({
+    id: product.id.toString(),
+  }));
 }
 
 export default function ProductPage({ params }: PageProps<Params>) {
